9d558a4f0c2b8fb89dbc64e5f8cf743512df7b06
pramadito <pramadito@gmail.com>
2025-09-22 14:27:27 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,11 @@ import { RoomRouter } from "./modules/room/room.router";
 import { SampleRouter } from "./modules/sample/sample.router";
 import "./config/passport";
 import passport from "passport";
-import { ReviewRouter } from "./modules/review/review.router";
+
 import { PropertyRouter } from "./modules/property/property.router";
+import { TransactionRouter } from "./modules/transaction/transaction.router";
+import { ReviewRouter } from "./modules/review/review.router";
+import { ReportRouter } from "./modules/report/report.router";
 
 
 export class App {
@@ -34,7 +37,11 @@ export class App {
     const profileRouter = new ProfileRouter();
     const propertyRouter = new PropertyRouter();
     const roomRouter = new RoomRouter();
+    const transactionRouter = new TransactionRouter();
     const reviewRouter = new ReviewRouter();
+    const reportRouter = new ReportRouter();
+
+    
 
     this.app.use("/samples", sampleRouter.getRouter);
     this.app.use("/auth", authRouter.getRouter());
@@ -42,6 +49,11 @@ export class App {
     this.app.use("/rooms", roomRouter.getRouter());
     this.app.use("/property", propertyRouter.getRouter());
     this.app.use("/reviews", reviewRouter.getRouter());
+    
+
+        this.app.use("/transactions", transactionRouter.getRouter());
+
+    this.app.use("/reports", reportRouter.getRouter());
 
     // Health check
     this.app.get("/health", (req, res) => {
